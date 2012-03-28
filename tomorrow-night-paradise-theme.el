da2a4653b6e332ba5c4fb78b604fc23214d435f9
Jim Myhrberg <contact@jimeh.me>
2012-03-28 16:07:20 +0100

    Make hl-line and highlight-indentation backgrounds a bit darker
    Now that comments are slightly brighter, hl-line can be brighter again
    as the comment text is more obvious even on the current line.

--- tomorrow-night-paradise-theme.el
+++ tomorrow-night-paradise-theme.el
@@ -37,7 +37,7 @@
    `(region ((t (:background ,selection))))
    `(fringe ((t (:background ,gray-2))))
    `(minibuffer-prompt ((t (:foreground ,blue))))
-   `(hl-line ((t (:background ,gray-4))))
+   `(hl-line ((t (:background ,gray-3))))
 
    ;; Font-lock stuff
    `(font-lock-builtin-face ((t (:foreground ,blue))))
@@ -91,8 +91,8 @@
    `(magit-item-highlight ((t (:background ,gray-2))))
 
    ;; highlight-indentation-mode
-   `(highlight-indentation-face ((t (:background ,gray-4))))
-   `(highlight-indentation-current-column-face ((t (:background ,gray-3))))
+   `(highlight-indentation-face ((t (:background ,gray-3))))
+   `(highlight-indentation-current-column-face ((t (:background ,gray-4))))
 
    ;; ECB
    `(ecb-default-highlight-face ((t (:background ,background :foreground ,red-2))))
